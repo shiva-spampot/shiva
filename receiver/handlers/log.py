1131c0ecf4302dff08939f1a262a8f2fef60499e
RahulBinjve <rahulbinjve12@gmail.com>
2013-09-19 16:41:26 +0530

    Removed unnecessary logging and old code.

--- receiver/handlers/log.py
+++ receiver/handlers/log.py
@@ -6,8 +6,5 @@ def log_handler():
   @route("(to)@(host)", to=".+", host=".+")
   @stateless
   def START(message, to=None, host=None):
-    logging.debug("MESSAGE to %s@%s:\n%s" % (to, host, str(message)))
-    #test_fd = open("/root/Desktop/LamsonHoneyMail/MyMailServer/test.txt", 'a')
-    #debugFd = open(spampot.cwd + '/' + spampot.debugFile, 'a')
-    #debugFd.write("[+] >>>>>>> Executing handlers.LOG \n")
-    #debugFd.close()
+    #logging.debug("MESSAGE to %s@%s:\n%s" % (to, host, str(message)))
+    logging.debug("MESSAGE to %s@%s" % (to, host))
